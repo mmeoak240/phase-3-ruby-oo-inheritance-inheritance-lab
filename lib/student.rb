1728d8ea1d16e85134980a494d7a66273c55780b
Michael Meoak <mmeoak240@gmail.com>
2022-06-24 15:41:43 -0700

    commit tree 448bf8ccca72c2dc9e87952b2d28073a4132f0c6

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative './user'
 
+class Student < User
+
+  attr_accessor 
+
+def initialize
+  @knowledge = []
+end
+
+def learn(string)
+self.knowledge << string
+end
+
+def knowledge
+  @knowledge
+end
 end
